f70de8bd66f5b5a291605dfc6310c93144bfafbc
GUSTAV0-C0STA <103291849+GUSTAV0-C0STA@users.noreply.github.com>
2023-08-28 21:57:53 -0300

    Aula 03

---
+++ Aula 03/acao.php
@@ -0,0 +1,6 @@
+<?php
+$_nome = $_POST['nome'];
+$_email = $_POST['_email'];
+$msg_messagem = $_POST['_mensagem']
+
+echo "<h1>" $_nome."=" .$_email;
---
+++ Aula 03/oi.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oi</title>
+</head>
+
+<body>
+    <?php echo "Olá Mundo" ?>
+    <?= "Olá Mundo" ?>
+    <?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+</body>
+
+</html>
